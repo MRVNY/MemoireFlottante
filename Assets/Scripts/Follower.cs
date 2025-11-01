9f1b78feb872a2eb33eca3aa815a9d045ad71f73
boussad <jamesx34craft@gmail.com>
2025-11-01 12:57:44 +0100

    New Object Movement System

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,17 @@
+using PathCreation;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+
+    public PathCreator pathCreator;
+    public float speed = 5;
+    float distanceTravelled;
+
+
+    void Update()
+    {
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+    }
+}
