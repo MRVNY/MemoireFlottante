b9db7fd56a568e8aa7c9003c165773a30ea0882c
QY <marvinyao@icloud.com>
2025-11-01 19:25:56 +0100

    Better flip & 5s timer

--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -17,6 +17,7 @@ public class WorldManager : MonoBehaviour
     private MeshCollider _downWorldCollider;
     
     private bool _flipping = false;
+    private bool _isDown = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -32,7 +33,7 @@ public class WorldManager : MonoBehaviour
     void Update()
     {
         //on f click flip the world input player settings
-        if(Keyboard.current.fKey.wasPressedThisFrame)
+        if(!_isDown && (Keyboard.current.fKey.wasPressedThisFrame || Gamepad.current.yButton.wasPressedThisFrame))
         {
             Flip();
         }
@@ -46,18 +47,28 @@ public class WorldManager : MonoBehaviour
 
         if (!_flipping)
         {
-            StartCoroutine(FlipAsync());
+            if (!_isDown)
+            {
+                _isDown = true;
+                StartCoroutine(FlipAsync());
+                
+                if(transform.eulerAngles.z < 90 || transform.eulerAngles.z > 270)
+                {
+                    //flipping to down world
+                    ObjectManager.Instance.OnFlipUnder();
+                }
+            }
             
-            if(transform.eulerAngles.z < 90 || transform.eulerAngles.z > 270)
-            {
-                //flipping to down world
-                ObjectManager.Instance.OnFlipUnder();
-            }
-            else
-            {
-                //flipping to up world
-                ObjectManager.Instance.OnFlipUp();
-            }
+            // if(transform.eulerAngles.z < 90 || transform.eulerAngles.z > 270)
+            // {
+            //     //flipping to down world
+            //     ObjectManager.Instance.OnFlipUnder();
+            // }
+            // else
+            // {
+            //     //flipping to up world
+            //     ObjectManager.Instance.OnFlipUp();
+            // }
         }
         
 
@@ -71,7 +82,9 @@ public class WorldManager : MonoBehaviour
         float elapsed = 0f;
         Vector3 originalRotation = transform.eulerAngles;
         Vector3 playerPos = Player.Instance.transform.position;
-        Vector3 rotCenter = new Vector3(playerPos.x, playerPos.y, 0);
+        Vector3 rotCenter = new Vector3(playerPos.x, 0, playerPos.z);
+        Vector3 cameraDir = Camera.main.transform.position - Player.Instance.transform.position;
+        Vector3 rotDic = new Vector3(cameraDir.x, 0, cameraDir.z).normalized;
         Player.Instance.SwitchFlip(true);
         
         _upWorldCollider.enabled = false;
@@ -81,19 +94,34 @@ public class WorldManager : MonoBehaviour
         {
             float step = angle * (Time.deltaTime / duration);
             //rotate around Player position
-            if(foward) transform.RotateAround(rotCenter, Vector3.forward, step);
-            else transform.RotateAround(rotCenter, Vector3.forward, -step);
+            transform.RotateAround(rotCenter, rotDic, step);
             // transform.Rotate(Vector3.forward, step);
             elapsed += Time.deltaTime;
             yield return null;
         }
         // Ensure final rotation is exact
         transform.eulerAngles = originalRotation + new Vector3(0, 0, angle);
+        transform.position = new Vector3(playerPos.x, 0, playerPos.z);
+        
         
         _upWorldCollider.enabled = true;
         _downWorldCollider.enabled = true;
         _flipping = false;
         Player.Instance.SwitchFlip(false);
+
+        if (_isDown)
+        {
+            StartCoroutine(CountSeconds());
+        }
+    }
+    
+    IEnumerator CountSeconds()
+    {
+        
+        yield return new WaitForSeconds(5f);
+        _isDown = false;
+        ObjectManager.Instance.OnFlipUp();
+        StartCoroutine(FlipAsync(false));
     }
     
     
