f475d1ea489d4b0eaa4c8ef3cc95cf5a5477d6bf
QY <marvinyao@icloud.com>
2025-11-02 11:43:01 +0100

    Title Screen

---
+++ Assets/Scripts/ObjectStory.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [Serializable]
+    public class ObjectStory
+    {
+        public GameObject obj;
+        public string name;
+        public string story;
+        
+    
+    }
+}
---
+++ Assets/TitleScreen.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TitleScreen : MonoBehaviour
+{
+    [SerializeField] GameObject creditsPanel; 
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        creditsPanel.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Gamepad.current != null)
+        {
+            if(Gamepad.current.aButton.wasPressedThisFrame)
+                OnStartClicked();
+            else if (Gamepad.current.yButton.wasPressedThisFrame)
+                OnCreditsClicked();
+            else if (Gamepad.current.bButton.wasPressedThisFrame && creditsPanel.activeSelf)
+                OnCloseCreditsClicked();
+                
+            
+        }
+        
+    }
+
+    public void OnStartClicked()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnCreditsClicked()
+    {
+        creditsPanel.SetActive(true);
+    }
+    
+    public void OnCloseCreditsClicked()
+    {
+        creditsPanel.SetActive(false);
+    }
+}
